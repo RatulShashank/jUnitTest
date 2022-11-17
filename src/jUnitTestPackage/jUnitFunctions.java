ca8f036980cb172d85a6f7dacc639342e3a15319
ratul <ratul@10.12.61.37>
2022-11-17 14:20:43 +0530

    jUnit

--- src/jUnitTestPackage/jUnitFunctions.java
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -1,5 +1,21 @@
 package jUnitTestPackage;
-
+import java.util.*;
 public class jUnitFunctions {
-
+static int AddNumbers(int a, int b) {
+	return a+b;
+}
+static String AddStrings(String a, String b) {
+	return a+b;
+}
+public static void main(String args[]) {
+	Scanner in = new Scanner(System.in);
+	int a,b;
+	String s1, s2;
+	a = in.nextInt();
+	b = in.nextInt();
+	s1 = in.nextLine();
+	s2 = in.nextLine();
+	System.out.println(AddNumbers(a,b));
+	System.out.println(AddStrings(s1, s2));
+}
 }
